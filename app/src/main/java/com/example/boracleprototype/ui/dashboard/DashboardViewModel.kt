5748d2694224bbb95f39583d6cb1d47061bcb109
Christopher <christopher.pishaki.533@my.csun.edu>
2024-03-25 17:41:14 -0700

    Updated some things to prepare for app demo (mainly text changes)

--- app/src/main/java/com/example/boracleprototype/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/boracleprototype/ui/dashboard/DashboardViewModel.kt
@@ -7,7 +7,7 @@ import androidx.lifecycle.ViewModel
 class DashboardViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
-        value = "This is dashboard Fragment"
+        value = "App Dashboard View Text <TEMP>"
     }
     val text: LiveData<String> = _text
 }
